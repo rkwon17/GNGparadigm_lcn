de976c0e6f9df1ec727571dff99048007ba1d9f5
Rachel Kwon <rkwon@wellesley.edu>
2021-02-04 15:25:32 -0500

    Update GNG7yr.R
    updated with new subject with full trial blocks

--- GNG7yr.R
+++ GNG7yr.R
@@ -11,15 +11,32 @@ library(plyr)
 setwd("/Volumes/dmc-nelson/Groups/DMC-Emotion-Project/Groups/Data/SevenYear/Behavioral/ProcessedData/GoNoGoScript/dropRAW")
 
 #---- read in .txt file
+#did s07 not come in ? 
 subj07 <- read_eprime("gonogo7year_final-007-1.txt")
 data07 <- FrameList(subj07)
 preview_levels(data07)
 preview_frames(data07)
-
+#question - what is the difference between RT triallist and triallist
 not_practice <- filter_in(data07, "Running", "RTtriallist")
 preview_levels(not_practice)
 not_practice_df <- to_data_frame(not_practice)
 str(not_practice_df)
 
-#types of responses - HIT/MISS/A????? What is A?
+triallist <- filter_in(data07, "Running", "triallist")
+preview_levels(triallist)
+triallist_df <- to_data_frame(triallist)
+
+#s011 
+subj011<- read_eprime("gonogo7year_final-011-1.txt")
+data011 <- FrameList(subj011)
+preview_levels(data011)
+preview_frames(data011)
+
+#based on the freq, trialist is the list with the go/no go 52 blocks
+
+
+#types of responses - 
+#A - anticipatory: RT less than 200ms
+#hit
+#miss
 

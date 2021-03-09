429640c77512db752ace4815ff4d1f092370113e
Rachel Kwon <rkwon@wellesley.edu>
2021-03-09 10:53:01 -0500

    added timing variables
    not 100% done / accurate

--- GNG7yr.R
+++ GNG7yr.R
@@ -7,26 +7,11 @@
 ####---potential libraries that may be helpful
 library(rprime)
 library(plyr)
-
+library(dplyr)
 setwd("/Volumes/dmc-nelson/Groups/DMC-Emotion-Project/Groups/Data/SevenYear/Behavioral/ProcessedData/GoNoGoScript/dropRAW")
 
-#---- read in .txt file
-#did s07 not come in ? 
-subj07 <- read_eprime("gonogo7year_final-007-1.txt")
-data07 <- FrameList(subj07)
-preview_levels(data07)
-preview_frames(data07)
-#question - what is the difference between RT triallist and triallist
-not_practice <- filter_in(data07, "Running", "RTtriallist")
-preview_levels(not_practice)
-not_practice_df <- to_data_frame(not_practice)
-str(not_practice_df)
-
-triallist <- filter_in(data07, "Running", "triallist")
-preview_levels(triallist)
-triallist_df <- to_data_frame(triallist)
-
-#s011 
+#---- read in .txt file------------------------------
+####-------s011 --------
 subj011<- read_eprime("gonogo7year_final-011-1.txt")
 data011 <- FrameList(subj011)
 preview_levels(data011)
@@ -40,6 +25,23 @@ triallist11_df <- to_data_frame(triallist_11)
 gotrials <- filter_in(data011, "Running", "RTtriallist")
 preview_levels(gotrials)
 gotrials <- to_data_frame(gotrials)
+
+###------s012
+subj012 <-read_eprime("gonogo7year_final-012-1.txt")
+data012 <- FrameList(subj012)
+triallist_12 <- filter_in(data012, "Running", "triallist")
+triallist12_df <- to_data_frame(triallist_12)
+
+###------drop other values---------------------------------
+
+#drop for go trials
+gotrials_drop <-subset(gotrials, Response!="Other") #confirm w JT that we want to drop all rows with Other (not counted in final n)
+
+#drop for GNG trials
+triallist12drop_df<- subset(triallist12_df,Response!="Other")
+
+
+####---------- summary and accuracy---------------------------
 
 gng_summary <- data.frame(matrix(nrow = 0, ncol = 12))
 colnames(gng_summary) <- c('subj','goHits','goMisses','goAnt','goOther','mixedHits', 'mixedMisses',
@@ -136,6 +138,43 @@ gng_summary$mixedCorrectPropPartial <- (gng_summary$mixedHits+gng_summary$mixedS
   (gng_summary$mixedHits+gng_summary$mixedMisses+gng_summary$mixedSI+gng_summary$mixedPI+gng_summary$mixedFI)
 
   
-#exclusionary criteria: 
-# how many trials? 
+###---- reaction time variables ------------------------------------------
+#figure out how to include into piping instead of hardcoding
+gotrials$MouseClick1RT = as.numeric(as.character(gotrials$MouseClick1RT))
+gotrials$MouseClick2RT = as.numeric(as.character(gotrials$MouseClick2RT))
+gotrials$MouseClick3RT = as.numeric(as.character(gotrials$MouseClick3RT))
+
+triallist11_df$MouseClick1RT = as.numeric(as.character(triallist11_df$MouseClick1RT))
+triallist11_df$MouseClick2RT = as.numeric(as.character(triallist11_df$MouseClick2RT))
+triallist11_df$MouseClick3RT = as.numeric(as.character(triallist11_df$MouseClick3RT))
+
+
+s011GoTiming <- gotrials%>% filter(Response=="HIT") %>%
+  mutate(MT=MouseClick2RT- MouseClick1RT) %>%
+  select(TrialType,Response,ISI, RT = MouseClick1RT,MT,TotalDur=Target.RT)
+  
+
+#gng trials
+
+s011GNGHIT<- triallist11_df %>% filter(Response=="HIT") %>%
+  mutate(MT=MouseClick2RT- MouseClick1RT) %>%
+  select(TrialType,Response,ISI, RT = MouseClick1RT,MT,TotalDur=Target.RT)
+
+#PI 
+
+s011GNG_PI<- triallist11_df %>% filter(Response=="PI") %>%
+  mutate(MT=MouseClick2RT- MouseClick1RT) %>%
+  select(TrialType,Response,ISI, RT = MouseClick1RT,MT,TotalDur=Target.RT)
+
+#FI
+
+s011GNG_FI<- triallist11_df %>% filter(Response=="FI") %>%
+  mutate(MT=MouseClick2RT- MouseClick1RT) %>%
+  select(TrialType,Response,ISI, RT = MouseClick1RT,MT,TotalDur=Target.RT)
+
+
+
+
+#notes about ISI - randomized from 300 to 1100 ms regardless of block #
+#block 1:
 
